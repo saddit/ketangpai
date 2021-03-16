dd7238eb81b737d9adba3504286da887dfec9efa
ShiJh <nnocreater@hotmail.com>
2021-03-16 18:25:49 +0800

    log

--- ketangpai.py
+++ ketangpai.py
@@ -1,5 +1,6 @@
 import random
 import time
+from datetime import datetime
 import service
 import logging
 import json
@@ -7,7 +8,7 @@ from login import login
 import session
 
 
-with open('config.json','r',encoding='utf-8') as f:
+with open('config.json', 'r', encoding='utf-8') as f:
     config = json.loads(f.read())
     semester = config['semester']
     term = config['term']
@@ -17,8 +18,13 @@ with open('config.json','r',encoding='utf-8') as f:
 
 
 def init_logging():
-    logging.getLogger().setLevel(logging.INFO)
-    logging.basicConfig(format='[%(levelname)s]:%(message)s')
+    t = datetime.now().strftime("%Y-%m-%d")
+    f = open(f'log/{t}.log','w',encoding='utf-8')
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(levelname)s,%(lineno)d]:%(message)s',
+        stream=f
+    )
 
 
 def run():
@@ -28,25 +34,38 @@ def run():
         courseid = course['id']
         logging.info(f'study {coursename}')
 
-        interact_list = service.get_interact_list(courseid)
+        try:
+            interact_list = service.get_interact_list(courseid)
+        except Exception as e:
+            logging.warning(f'{coursename} error:{e}')
+            continue
+
         for interact in interact_list:
             interactid = interact['id']
             interact_title = interact['title']
-
-            studyStatus: bool = service.check_study_status(interactid)
-
-            # 已经学习完则跳过
-            if studyStatus:
-                logging.info(f'[{coursename}]: skip \"{interact_title}\"')
+            try:
+                studyStatus: bool = service.check_study_status(interactid)
+                # 已经学习完则跳过
+                if studyStatus:
+                    logging.info(f'[{coursename}]: skip \"{interact_title}\"')
+                    continue
+                pageCount = service.get_page_count(interactid)
+            except Exception:
+                logging.warning(f'[{coursename}]:{interact_title} error:{e}')
                 continue
-
-            pageCount = service.get_page_count(interactid)
 
             # 遍历ppt记录学习进度
             for i in range(1, pageCount + 1):
-                msg = service.study_page(interactid, i)
-                logging.info(f'[{coursename},{interact_title}]: page {i} {msg}')
+                try:
+                    msg = service.study_page(interactid, i)
+                except Exception as e:
+                    logging.warning(
+                        f'[{coursename}]:{interact_title} page{i} error:{e}')
+                    continue
+                logging.info(
+                    f'[{coursename},{interact_title}]: page {i} {msg}')
                 time.sleep(0.5 + random.uniform(-0.1, 0.2))
+
             logging.info(f'[{coursename}]: finish {interact_title}')
 
         logging.info(f'finished {coursename}')
@@ -67,9 +86,6 @@ if __name__ == '__main__':
     if token != config['token']:
         config['token'] = token
         logging.info('正在保存token')
-        with open('config.json','w',encoding='utf-8') as f:
-            f.write(json.dumps(config,separators=4))
+        with open('config.json', 'w', encoding='utf-8') as f:
+            f.write(json.dumps(config, separators=4))
         logging.info('保存token成功')
-
-    
-
